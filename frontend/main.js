ae6749d1b0aa065492b564db23fff253ec72d44b
venugopal9955 <venugopalguttikonda2002@gmail.com>
2025-07-05 16:16:08 +0530

    changed host

--- frontend/main.js
+++ frontend/main.js
@@ -120,7 +120,7 @@ map.on('click', function(e) {
     const formData = new FormData(this);
     const data = Object.fromEntries(formData.entries());
 
-    fetch('http://localhost:5000/api/properties', {
+    fetch('/api/properties', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
